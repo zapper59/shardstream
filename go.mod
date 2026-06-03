74dce81e4822e4b6931a5cea804e448dd237d96d
Brian Richer <brian.mathew.richer@gmail.com>
2026-06-02 23:33:23 -0700

    Build minimal handshake functionality

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module shardstream
 
 go 1.26.3
+
+require github.com/akamensky/argparse v1.4.0 // indirect
--- shardstream.go
+++ shardstream.go
@@ -1,26 +1,72 @@
 package main
 
 import (
+    "bufio"
     "encoding/binary"
     "fmt"
+    "github.com/akamensky/argparse"
     "io"
     "log"
     "net"
     "os"
 )
 
+type ParsedArgs struct {
+    listenPort int
+    coordinatorCommand *argparse.Command
+    peerCommand *argparse.Command
+    coordinatorHost *string
+}
+
 func main() {
-    port := os.Args[1]
-    fmt.Println("Listening on port: " + port)
-    runCoordinator(port)
+    parsedArgs := parseArgs()
+
+    if parsedArgs.coordinatorCommand.Happened() {
+        runCoordinator(parsedArgs.listenPort)
+    } else if parsedArgs.peerCommand.Happened() {
+        runPeer(parsedArgs)
+    } else {
+        log.Fatal("Unexpected lack of subcommand!")
+    }
+}
+
+func parseArgs() (ParsedArgs) {
+    parser := argparse.NewParser(
+        "shardstreamTerminal",
+        "Start a node to participate in a distributed terminal based broadcast.",
+    )
+    listenPort := parser.Int(
+        "l",
+        "listenPort",
+        &argparse.Options{Required: true, Help: "The port to accept incoming requests on."},
+    )
+
+    coordinator := parser.NewCommand("coordinator", "Start a root of a broadcast tree.")
+
+    peer := parser.NewCommand("peer", "Start a participant in a broadcast tree.")
+    coordinatorHost := peer.String(
+        "c",
+        "coordinatorHost",
+        &argparse.Options{
+            Required: true, Help: "The <hostname>:<port> to contact the coordinator at.",
+        },
+    )
+
+    if err := parser.Parse(os.Args); err != nil {
+        log.Fatal(parser.Usage(err))
+    }
+
+    return ParsedArgs{*listenPort, coordinator, peer, coordinatorHost}
 }
 
 type HandshakeInfo struct {
     peerListeningOn int
 }
 
-func runCoordinator(port string) {
-    listener, err := net.Listen("tcp", ":" + port)
+func runCoordinator(port int) {
+    portStr := fmt.Sprintf("%d", port)
+    fmt.Println("Listening on port: " + portStr)
+    listener, err := net.Listen("tcp", ":" + portStr)
     if err != nil {
         log.Fatal(err)
     }
@@ -31,11 +77,22 @@ func runCoordinator(port string) {
         if conn, err := listener.Accept(); err != nil {
             log.Println(err)
         } else {
-            if info, err := receiveHandshake(conn); err != nil {
-                log.Println(err)
-            } else {
-                fmt.Printf("%d", info.peerListeningOn)
-            }
+            go handleConnection(conn)
+        }
+    }
+}
+
+func handleConnection(conn net.Conn) {
+    defer conn.Close()
+
+    if info, err := receiveHandshake(conn); err != nil {
+        log.Println(err)
+    } else {
+        fmt.Printf("Peer is listening on: %d", info.peerListeningOn)
+
+        if _, err := conn.Write([]byte("Stream page 1\n")); err != nil {
+            log.Println("Failed to stream page 1")
+            log.Println(err)
         }
     }
 }
@@ -50,3 +107,26 @@ func receiveHandshake(conn net.Conn) (*HandshakeInfo, error) {
     return info, nil
 }
 
+func sendHandshake(conn net.Conn, info HandshakeInfo) (error) {
+    currentWord := make([]byte, 8)
+    binary.BigEndian.PutUint64(currentWord, uint64(info.peerListeningOn))
+    _, err := conn.Write(currentWord)
+    return err
+}
+
+func runPeer(args ParsedArgs) {
+    conn, err := net.Dial("tcp", *args.coordinatorHost)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    if err := sendHandshake(conn, HandshakeInfo { args.listenPort } ); err != nil {
+        log.Fatal(err)
+    }
+
+    page, err := bufio.NewReader(conn).ReadString('\n')
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(page)
+}
